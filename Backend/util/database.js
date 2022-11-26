63d1d562aa96c1993fdd2627b75ab178e1c62482
MujaffarYadawad <mujaffaryadawad587313@gmail.com>
2022-11-26 12:31:45 +0530

    full stack appointment app with async await

---
+++ Backend/util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize=new Sequelize('node-complete','root','Mujaffar@MH2007',{
+    dialect:'mysql',
+    host:'localhost'});
+
+
+module.exports=sequelize;    
